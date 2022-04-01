baa0f82af4d7ec2163d0099486c9ba47e8cdab83
Yana Inozemtseva <sahl21324@gmail.com>
2022-04-02 01:48:51 +0300

    addingInformationAboutMembers

---
+++ MovieForYou/Model/ModelOfMembers.swift
@@ -0,0 +1,29 @@
+//
+//  File.swift
+//  MovieForYou
+//
+//  Created by Яна Иноземцева on 02.04.2022.
+//
+
+import Foundation
+
+struct Member {
+    let name: String
+    let surname: String
+    let info: String
+    
+    var fullName: String {
+        "\(name) \(surname)"
+    }
+    
+    static func getMemberList() -> [Member] {
+        [
+            Member(name: "Yana", surname: "Inozemtseva", info: "22 года. IT-рекрутер в прошлом. Хобби - танцы, растяжка, путешествия."),
+            Member(name: "Mikhail", surname: "Svetlov", info: "Нейл-мастер, микроэлектронщик, организатор мероприятий, эпо-арт художник, чайный мастер, начинающий iOS разработчик, и просто скромный парень :3"),
+            Member(name: "Marat", surname: "Khayrullin", info: "Не важно что говорят крысы за спиной у кисы"),
+            Member(name: "Leo", surname: "Tyrko", info: "Бизнесмен, программис. Хобби - спорт"),
+            Member(name: "Dmitry", surname: "Kulczycki", info: "Инженер по эксплуатации зданий и сооружений. Хобби - активный отдых, баскетбол, футбол"),
+           
+        ]
+    }
+}
---
+++ MovieForYou/ViewControllers/ListOfMembers.swift
@@ -0,0 +1,70 @@
+//
+//  ListOfMembers.swift
+//  MovieForYou
+//
+//  Created by Яна Иноземцева on 02.04.2022.
+//
+
+import UIKit
+
+class ListOfMembers: UITableViewController {
+
+    private var memberList = Member.getMemberList()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.leftBarButtonItem = editButtonItem
+        tableView.rowHeight = 80
+       
+
+    }
+
+    // MARK: - Table view data source
+
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        memberList.count
+    }
+
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "member", for: indexPath)
+        let member = memberList[indexPath.row]
+        var content = cell.defaultContentConfiguration()
+        content.text = member.fullName
+        content.image = UIImage(named: member.fullName)
+        content.imageProperties.cornerRadius = tableView.rowHeight / 2
+    
+        cell.contentConfiguration = content
+
+        return cell
+    }
+   
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let memberDetailsVC = segue.destination as? MoreInformationAboutMembers else { return }
+        memberDetailsVC.member = sender as? Member
+    }
+
+}
+
+extension ListOfMembers {
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let member = memberList[indexPath.row]
+        performSegue(withIdentifier: "segueyInDetails", sender: member)
+    }
+    
+    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        .none
+    }
+    
+    override func tableView(_ tableView: UITableView, shouldIndentWhileEditingRowAt indexPath: IndexPath) -> Bool {
+        false
+    }
+    
+    override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        let member = memberList.remove(at: sourceIndexPath.row)
+        memberList.insert(member, at: destinationIndexPath.row)
+    }
+}
+
